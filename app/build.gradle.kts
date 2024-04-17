d9fc2d045fb1c34473bc2f3693319f4fcddab565
corneliuze <falaycornelius@gmail.com>
2024-04-17 19:24:53 +0100

    initial commit with web view and sdk setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.insomnia.iccfanpassport"
+    namespace = "com.iccfan.passport"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.insomnia.iccfanpassport"
+        applicationId = "com.iccfan.passport"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -59,6 +59,10 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(project(mapOf("path" to ":fanpassport")))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
